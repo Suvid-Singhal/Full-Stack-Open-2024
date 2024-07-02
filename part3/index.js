54fe45e854ba65e69590f496499cf91d1b3fc983
Suvid Singhal <anooptest100@gmail.com>
2024-07-02 15:17:02 +0530

    Completed ex 3.11

--- part3/index.js
+++ part3/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(express.static('build'))
 //app.use(morgan("tiny"))
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'))
+app.use(cors())
 
 let persons = [
     { 
@@ -82,7 +85,7 @@ app.post('/api/persons', (request, response) => {
     }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
